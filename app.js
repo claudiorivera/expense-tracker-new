54f432a0ddbfec09205e1fd021d7d92b8f57f133
Claudio Rivera <me@claudiorivera.com>
2020-05-24 11:27:10 -0500

    Basic setup

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config({ path: "./config/.env" });
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/users", usersRouter);
+
+module.exports = app;
