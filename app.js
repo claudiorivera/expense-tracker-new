0be922a2bb6b8ba1b23e3c6e84bf315f1fac7271
Claudio Rivera <me@claudiorivera.com>
2020-05-26 23:40:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "public")));
 
 // Db connect
 require("./config/db")();
