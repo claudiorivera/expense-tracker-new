8158a74e4c036b8e18bc64cdd608f6d4f7754341
Claudio Rivera <me@claudiorivera.com>
2020-05-26 21:42:19 -0500

    Added default get route for Heroku deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
 const logger = require("morgan");
+const path = require("path");
 
 const app = express();
 app.use(logger("dev"));
@@ -12,5 +13,10 @@ require("./config/db")();
 
 const transactionsRouter = require("./routes/transactions");
 app.use("/transactions", transactionsRouter);
+
+// https://coursework.vschool.io/deploying-mern-with-heroku/
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
