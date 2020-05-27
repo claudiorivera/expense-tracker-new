573bb11b1a0d67a7f25cea7ea549a25fc385cafd
Claudio Rivera <me@claudiorivera.com>
2020-05-27 00:11:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,15 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "client", "public")));
+
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "client", "build")));
+}
 
 // Db connect
 require("./config/db")();
 
 const transactionsRouter = require("./routes/transactions");
 app.use("/transactions", transactionsRouter);
-
-// https://coursework.vschool.io/deploying-mern-with-heroku/
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 module.exports = app;
